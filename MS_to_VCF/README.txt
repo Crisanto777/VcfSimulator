7b4fa30456aa8d7a4befd0160b7e0fd20f2e390b
Crisanto777 <Samuel_Angelo_Crisanto@brown.edu>
2016-03-22 15:07:11 -0400

    added ability to create blockfiles and unphased genotypes

--- MS_to_VCF/README.txt
+++ MS_to_VCF/README.txt
@@ -1,9 +1,10 @@
 This script takes as input the output of the hudson simulator.
 This is generated with a command similar to
 
-ms 4 10 -t 5.0 -s 500
+ms 4 10 -t 5.0 -s 500 > simdata.txt
 
-where we make 4 haplotypes that each contain 500 alleles.
+where we make 4 haplotypes that each contain 500 alleles and 
+pipe the output to simdata.txt
 
 Note that it expects at least 4 haplotypes in the file in order to simulate
 a family. If more than 4 is provided, it only takes the first 4.
@@ -12,3 +13,15 @@ a family. If more than 4 is provided, it only takes the first 4.
 This can be run with the command
 
 python ms_to_vcf.py -i ../data/<filename.txt> -o ../output
+
+mandatory arguments:
+	-i --input <(relative) path to .txt containing output of ms simulator>
+	-o --output <(relative) path to folder to put output files> 
+
+optional arguments:
+	-fathername (String, default=NA0001)
+	-mothername (String, default=NA0002)
+	-childname (String, default=NA0003)
+	-phased (True/False, default=False))
+	-blockfile (True/False, default=False)
+	-blockdensity (int, default=5)
--- MS_to_VCF/src/ms_to_vcf.py
+++ MS_to_VCF/src/ms_to_vcf.py
@@ -1,13 +1,19 @@
 import argparse
 import datetime
 import random
-import derive_genotypes
+from scipy.stats import poisson
 
 
 def setup_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-input', '-i', help="The file containing the output of a call to ms that has at least 4 haplotypes")
     parser.add_argument('-output', '-o', help="The name of the folder to place the output.")
+    parser.add_argument('-phased', '-ph', default=False, help="Whether you want the vcf to be phased or unphased")
+    parser.add_argument('-blockfile', '-b', default=False, help="Whether you want the program to spoof a hapcompass blockfile")
+    parser.add_argument('-blockdensity', '-bd', default=5, help="Expected number of blocks per 100 SNPs")
+    parser.add_argument('-fathername', '-f', default="NA0001", help="name of father files, without file extension")
+    parser.add_argument('-mothername', '-m', default="NA0002", help="name of mother files, without file extension")
+    parser.add_argument('-childname', '-c', default="NA0003", help="name of child files, without file extension")
     
     return parser
 
@@ -42,7 +48,7 @@ def print_sequence(geno):
         print ''.join(gen).rstrip()
         
 
-def writeVcf(path, genome_name, snp_identity_hash, hapseq1, hapseq2):
+def writeVcf(path, genome_name, snp_identity_hash, hapseq1, hapseq2, is_phased):
 
     # genome_name is something of the form NA0001 etc
     # can set this to false if need to debug
@@ -78,22 +84,27 @@ def writeVcf(path, genome_name, snp_identity_hash, hapseq1, hapseq2):
                 snp_var_1 = snp_identity_hash[i]["variant"][1]
                 hap1 = hapseq1[i]
                 hap2 = hapseq2[i]
-                vcf_row = generateVcfRow(chrom_num, snp_pos, snp_id, snp_var_0, snp_var_1, hap1, hap2)
+                vcf_row = generateVcfRow(chrom_num, snp_pos, snp_id, snp_var_0, snp_var_1, hap1, hap2, is_phased)
                 f.write(vcf_row)
 
 
-def generateVcfRow(chrom_num, snp_pos, id, ref, alt, hap1, hap2):
+def generateVcfRow(chrom_num, snp_pos, input_id, ref, alt, hap1, hap2, is_phased):
     # for more info look at http://www.1000genomes.org/wiki/Analysis/vcf4.0
 
     chrom_num = str(chrom_num)
     snp_pos = str(snp_pos)
-    my_id = str(id)
+    my_id = str(input_id)
     ref = str(ref)
     qual = "99"
     my_filter = "PASS"
     info = "."
     my_format = "GT:GQ:DP:HQ"
-    alleles = str(hap1) + "|" + str(hap2) 
+
+    if is_phased:
+        alleles = str(hap1) + "|" + str(hap2) 
+    else:
+        alleles = str(hap1) + "/" + str(hap2)
+
     formatted_info = str(alleles + ":"
                       + "48" + ":"
                       + "1" + ":" 
@@ -120,10 +131,69 @@ def simulate_variants():
 
     return option_list
 
-
-
-def simulate_family(output, haps, num_de_novo_mutations=0):
+def write_blockfile(path, file_name, hap1, hap2, snp_identity_hash, block_density):
+    #choose a random number of blocks, distributed poisson, dependent on the length of the haplotype
+    #uses block_density (expected number of blocks per 100 SNPs)
+    #this number is fudged - i am unsure about the actual mean or distribution
+    num_blocks = poisson.rvs((float(len(hap1))/100)*block_density)
+    
+    #for each block,
+    #    choose a position in the genome, distributed at random
+    positions = []
+    for i in xrange(int(num_blocks)):
+        positions.append(random.randint(0, len(hap1)-5))
         
+    #sort the positions, since the output file has them in increasing order
+    position_set = set(positions)
+    positions = list(position_set)
+    #eliminate duplicate positions
+    positions = sorted(positions)
+    
+    #for each position:
+    #    choose a random number of snps,
+    #       distributed poisson(1) + 2 (since min block length should be 2) 
+    #    this number is fudged - i am unsure about the actual mean or distribution
+    
+    newPath = path + "/" + file_name + ".txt"
+    print "creating file:", newPath
+    with open(newPath, 'w') as f:
+        for position in positions:
+            block_len = poisson.rvs(1) + 2
+
+            first_block_line = []
+            first_block_line.append("BLOCK")
+            first_block_line.append(str(snp_identity_hash[position]['snp_pos']))
+            #can't phase off the end of the chromosome
+            if ((position + block_len) >= len(hap1)):
+                block_len = len(hap1)-position-2
+            first_block_line.append(str(snp_identity_hash[position+block_len-1]['snp_pos']))
+            first_block_line.append("*") #start position in hapcompass out vcf
+            first_block_line.append("*") #end position in hapcompass out vcf
+            first_block_line.append("*") #unclear 
+            first_block_line.append(str(snp_identity_hash[position]['chrom_num']))
+
+            f.write("\t".join(first_block_line) + "\n")
+
+            for block_snp in xrange(block_len):
+                snp_num = position+block_snp
+                line_output = []
+                line_output.append("VAR_POS_"+str(snp_identity_hash[snp_num]["snp_pos"]))
+                line_output.append(str(snp_identity_hash[snp_num]["snp_pos"]))
+                line_output.append("*")
+                line_output.append(str(hap1[snp_num]))
+                line_output.append(str(hap2[snp_num]))
+                f.write('\t'.join(line_output) + "\n")
+            f.write('\n')
+
+def simulate_family(args, haps, num_de_novo_mutations=0):
+    father_file_name = args.fathername
+    mother_file_name = args.mothername
+    child_file_name = args.childname
+    
+    output = args.output
+    is_phased = (args.phased == "True")
+    make_blockfiles = (args.blockfile == "True")
+
     # generate and keep track of snp positions, id's, etc for consistency across all vcf's
     #    this is the snp_identity_hash
     rng = random.randint
@@ -142,11 +212,11 @@ def simulate_family(output, haps, num_de_novo_mutations=0):
     # this takes the first four haplotypes and assumes that 1 and 2 are father, and 3 and 4 are mother
 
     # first, arbitrarily assume the first two are the father
-    writeVcf(output, "NA0001", snp_identity_hash, haps[0], haps[1])
+    writeVcf(output, father_file_name, snp_identity_hash, haps[0], haps[1], is_phased)
 
     # then, assume the next two are the other
     # generate the mother using the same id's generated by the father
-    writeVcf(output, "NA0002", snp_identity_hash, haps[2], haps[3])
+    writeVcf(output, mother_file_name, snp_identity_hash, haps[2], haps[3], is_phased)
 
     # it then simulates a child by choosing one haplotype from the father and one from the mother
     c1_index = rng(0, 1) 
@@ -164,7 +234,7 @@ def simulate_family(output, haps, num_de_novo_mutations=0):
         de_novo_muts = set()
         for i in xrange(num_de_novo_mutations):
             hap = rng(0, 1)
-            pos = rng(0, len(childhaps[0]))
+            pos = rng(0, len(childhaps[0])-1)
             while (hap, pos) in de_novo_muts:
                 hap = rng(0, 1)
                 pos = rng(0, len(childhaps[0]))
@@ -172,8 +242,8 @@ def simulate_family(output, haps, num_de_novo_mutations=0):
                 childhaps[hap][pos] = "1"
             else:
                 childhaps[hap][pos] = "0" 
-            f.write("de novo mutation at\t" + str(hap) + "," +
-                    " POS = " + str(snp_identity_hash[pos]["snp_pos"]) +
+            f.write("de novo mutation at\t" + str(hap) + "," + 
+                    " POS = " + str(snp_identity_hash[pos]["snp_pos"]) + 
                     " ID = " + str(snp_identity_hash[pos]["snp_id"]) + "\n");
             de_novo_muts.add((hap, pos))
 
@@ -181,11 +251,20 @@ def simulate_family(output, haps, num_de_novo_mutations=0):
         # simulate crossover event
 
         f.write(child_changes)
-        writeVcf(output, "NA0003", snp_identity_hash, childhaps[0], childhaps[1])
-
+        writeVcf(output, child_file_name, snp_identity_hash, childhaps[0], childhaps[1], is_phased)
+        
+        if make_blockfiles:
+            """
+            for father, mother, and child
+                make block file
+            """
+            write_blockfile(output, father_file_name, haps[0], haps[1], snp_identity_hash, float(args.blockdensity))
+            write_blockfile(output, mother_file_name, haps[2], haps[3], snp_identity_hash, float(args.blockdensity))
+            write_blockfile(output, child_file_name, childhaps[0], childhaps[1], snp_identity_hash, float(args.blockdensity))
+    
 if __name__ == "__main__":
     parser = setup_parser()
     args = parser.parse_args()
     haps = load_haps(args.input)
     num_de_novo_mutations = 2
-    simulate_family(args.output, haps, num_de_novo_mutations)    
+    simulate_family(args, haps, num_de_novo_mutations)    
